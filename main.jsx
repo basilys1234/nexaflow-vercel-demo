f9ebb0d19214f908951ed4f2aac2930f5ccbd343
basilys1234 <basilys1234@gmail.com>
2025-08-13 13:34:43 +0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import NexaFlowClinicChatbotDemo from './NexaFlowClinicChatbotDemo.jsx'
-
-createRoot(document.getElementById('root')).render(<NexaFlowClinicChatbotDemo />)
